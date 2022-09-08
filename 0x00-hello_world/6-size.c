a68d4aafcf98d43915a4a15785764837045a57f5
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-08 14:56:29 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
+{ #include <stdio.h>
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if the exited properly, non-zero otherwise
  */
 int main (void)
-{
-	int a; long int b; long long int c; char d; float f;
+
+{int a; long int b; long long int c; char d; float f;
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-}	
+}}	
