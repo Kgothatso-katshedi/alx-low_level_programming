fbcf9b11e6b3a77fa52f1f5ff8672adaabf3ccf6
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-08 11:56:08 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,3 @@
 #include <stdio.h> 
-/* program should end with the value 0 *//* program should end with the value 0 *//* program should end with the value 0 */
-int main(void) 
-{ 
-    puts("Programming is like building a multilingual puzzle\n");
-    return(0); 
-}
+/* program should end with the value 0 */
+int main(void){puts("\"Programming is like building a multilingual puzzle\n");return(0);}
