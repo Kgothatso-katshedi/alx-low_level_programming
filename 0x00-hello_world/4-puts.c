caea984676f2bd4236eb070ab0748f615412fddf
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-08 13:08:40 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
-#include <stdio.h>       
+#include <stdio.h>
+
 /**
- *main - entry point
- *
- *Description:Using main fuction
- *Return:                       
- */
+*
+* main - entry point
+*
+* Description:Using main fuction
+* Return:                        
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }	
