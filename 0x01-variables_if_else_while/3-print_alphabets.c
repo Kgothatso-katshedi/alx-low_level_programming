005daea836a915bb7b2fc7e5b62ad5a1c6664a09
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-09 13:42:48 -0700

    print alhabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,24 +5,17 @@
  * Return: Always 0 (Success/correct)
  */
 int main(void)
-
 {
-char lower_case
-char upper_case
+	char lower_case
+	char upper_case
 
-for (lower_case = 'a'; lower_case <= 'z'; lowe_case++)
-{
-putchar(lower_case);
-}
+	for (lower_case = 'a'; lower_case <= 'z'; lowe_case++)
+		putchar(lower_case);
 
-for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-{
-putchar(upper_case);
-}
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+		putchar(upper_case);
 
+	putchar('\n');
 
-putchar('\n');
-
-return (0)
-
+	return (0)
 }	
