fbaf29cf7c4aef256a0156038e9d9af8f32f16dd
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-13 19:51:55 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ *  Return: Always 0 (Succes)
+ */
+void print_alphabet(void)
+{
+	int k;
+	
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(k);
+	}	
+	_putchar('\n');
+}
