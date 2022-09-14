6cb440dec273fb40c17bbb70974193202119378f
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-14 06:53:25 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,31 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		for (n = n; n <= 97; n++)
-			printf("%d,", n);
-		printf("-98\n");
+		if (n < 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
 	}
-	else
-		for (n = n; n > 98 ; n--)
-			printf("%d,", n);
-	printf("98\n");
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+		else
+		{
+			printf("98");
+		}
+		printf("\n");
+	}
 }
