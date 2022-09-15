881728fa5802e4485a17a1207dd4d4ee7c123a55
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-15 04:58:01 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - uppercase letters
+ * _isupper - checks for uppercase letters
  * @c: character to check
- *
- * @c: character to check
+ * Return: 0 or 1
  */
 
 int _isupper(int c)
-
 {
-	if (c >= 'A' && c <= 'Z')
-
+	if ((c >= 'A' && c <= 'Z'))
+	{
 		return (1);
-	else
+	}
 
-		return (0);
+	return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,22 @@
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *main - header file for prototype
+ *void prototype (void)
+ *int prototype (int)
+ */
+
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);
+void prin_line(int n);
 void print_diagonal(int n);
-void print_square(int size);
+void print_square(int n);
 void print_triangle(int size);
+void print_nuber(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
