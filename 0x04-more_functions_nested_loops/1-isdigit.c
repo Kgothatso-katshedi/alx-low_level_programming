53df15aa96bbd966fbee303cc3aae30cb3eacf3f
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-15 05:26:36 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
  * _isdigit - checks for characters that are digits
  * @c: character 
  * Return: 0 or 1
  */
-
 int _isdigit(int c)
 {
 	if ((c >= 48) && (c <= 57))
 	{	
 		return (1);
 	}
-		
 	return (0);
 }
