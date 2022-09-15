00ce0aa7586ac23f9dce6186357346f61e4097ca
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-15 01:31:19 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - FizzBuzz
+ *
+ * Description: prints the numbers 1 - 100 w/
+ * and FizzBuzz for multiples of both
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; 1<= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
