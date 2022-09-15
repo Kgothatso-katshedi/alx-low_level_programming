1795ce485e956010ed6902ef4973dc71463986ea
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-09-15 04:20:23 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,18 @@
 
 /**
  * _isupper - uppercase letters
- * 
  * @c: character to check
- * Description: define if a character is a number
- * Return: 1 yes 0 no
+ *
+ * @c: character to check
  */
 
 int _isupper(int c)
+
 {
 	if (c >= 'A' && c <= 'Z')
+
 		return (1);
 	else
+
 		return (0);
 }
