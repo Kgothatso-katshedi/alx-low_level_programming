3d9b0ece400a616792137d5c10969a6fc50b0c05
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-10-10 04:35:16 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/** main - prints the name of the file it was compiled from
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
