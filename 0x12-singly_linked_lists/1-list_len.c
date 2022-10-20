4f54367feca85b8ddeed53a66773836a974f37b3
Kgothatso.katshedi <kgothatso.katshedi@gmail.com>
2022-10-20 03:41:26 -0700

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t
+ * list
+ * @h: Const pointer of linked list_t list
+ * Return: Number of nodes
+ */
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	
+	return (i);
+}
+
